bdee81293b4d2ef24264b2c84dc37e125f087cb8
Aaron Jackson <aarontjackson@outlook.com>
2019-10-02 20:15:21 -0400

    first message ID of product successfully runs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,31 +2,98 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "rootroot",
-  database: "bamazon"
+    // Your password
+    password: "rootroot",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log(`\x1b[33mWelcome user ${connection.threadId}.\x1b[33m Please see the available items below!`);
     queryAllProducts();
-  });
-  
-  function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(`\x1b[36m ${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price} | ${res[i].stock_quantity} \x1b[36m`);
-      }
-      console.log("\x1b[47m-----------------------------------\x1b[47m");
+    buyProduct();
+});
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(`\x1b[36m ${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price} | ${res[i].stock_quantity} \x1b[36m`);
+        }
+        console.log("-----------------------------------");
     });
-  }
+}
+
+function buyProduct() {
+    // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_id);
+            }
+            return choiceArray;
+          },
+          message: "What is the ID of the product you would like to buy?"
+        },
+        {
+          name: "units",
+          type: "input",
+          message: "How many units of the product would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if bid was high enough
+        if (chosenItem.stock_quantity >= parseInt(answer.units)) {
+
+        // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: chosenItem.stock_quantity - answer.units
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Purchase was successful!");
+              start();
+            }
+          );
+        }
+        else {
+          // bid wasn't high enough, so apologize and start over
+          console.log("Your bid was too low. Try again...");
+          start();
+        }
+      });
+  });
+    
+  };
+
